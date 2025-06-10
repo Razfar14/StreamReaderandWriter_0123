23c336fb7e5335e0a6791c5ce692c55b77e0877a
Razfar14 <rajarangkuty14@gmail.com>
2025-06-10 18:03:24 +0700

    menunjukan ke sebuah nama file

--- ioFile.cpp
+++ ioFile.cpp
@@ -7,6 +7,9 @@ int main(){
     string baris;
     // membuka file dalam mode menulis
     ofstream outfile;
+    //menunjukan ke sebuah nama file
+    outfile.open("contohfile.txt");
+    cout << "Menulis file, \'q\' untuk keluar" << endl;
     }
         //tutup file setelah selesai
     }
