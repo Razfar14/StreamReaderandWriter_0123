e352a0702a88ef568d6723e96af984e6e80d30c9
Razfar14 <rajarangkuty14@gmail.com>
2025-06-10 18:03:07 +0700

    membuka file dalam mode menulis

--- ioFile.cpp
+++ ioFile.cpp
@@ -3,5 +3,13 @@
 #include <string>
 using namespace std;
 
-
+int main(){
+    string baris;
+    // membuka file dalam mode menulis
+    ofstream outfile;
+    }
+        //tutup file setelah selesai
+    }
+    
+ }
      
